1ef42e097a014fbc78121212bde2d1dc606c03c8
jacques-oliveira <jacques.oliveira.eng@gmail.com>
2024-11-14 14:17:57 -0300

    redução de épocas, redução de camadas, melhor resultado

--- ml-doenca-cardiaca.py
+++ ml-doenca-cardiaca.py
@@ -7,7 +7,7 @@ Created on Tue Nov 12 22:36:22 2024
 """
 import pandas as pd
 import matplotlib.pyplot as plt
-pd.set_option('display.max_columns',1000)
+#pd.set_option('display.max_columns',1000)
 #%%
 entradas = pd.read_csv('entradas_Parte2 - entradas_Parte2.csv')
 saida = pd.read_csv('saidas_Parte2 - saidas_Parte2.csv')
@@ -43,25 +43,23 @@ from keras.layers import Dense, Dropout
 from keras.layers import Dense
 from keras.regularizers import l2
 from keras.layers import BatchNormalization
+from keras.initializers import HeNormal
 #%%
 
 classificador = Sequential()
-classificador.add(Dense(units = 64, input_dim = 13
-                        ,kernel_initializer='random_uniform',
-                        activation = 'relu',kernel_regularizer=l2(0.01)))
+classificador.add(Dense(units = 128, input_dim = 13
+                        ,kernel_initializer=HeNormal(),
+                        activation = 'relu',kernel_regularizer=l2(0.005)))
 classificador.add(BatchNormalization())  # Adicionando Batch Normalization
-#classificador.add(Dropout(0.2))  # Dropout para regularização
+classificador.add(Dropout(0.35))  # Dropout para regularização
 
-classificador.add(Dense(units = 32
-                        ,kernel_initializer='random_uniform',
+classificador.add(Dense(units = 24
+                        ,kernel_initializer=HeNormal(),
                         activation = 'relu'))
-
-classificador.add(Dense(units = 48
-                        ,kernel_initializer='random_uniform',
-                        activation = 'relu'))
+classificador.add(Dropout(0.2))  # Dropout para regularização
 
 classificador.add(Dense(units = 1
-                        ,kernel_initializer='random_uniform',
+                        ,kernel_initializer=HeNormal(),
                         activation = 'sigmoid'))
 
 #classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy',metrics = ['binary_accuracy'])
@@ -73,7 +71,7 @@ classificador.compile(optimizer=AdamW(learning_rate=0.00001, weight_decay=1e-4),
                       metrics=['binary_accuracy'])
 #%%
 #Treinando modelog
-classificador.fit(previsores_treinamento, classe_treinamento, batch_size = 10, epochs = 5000)
+classificador.fit(previsores_treinamento, classe_treinamento, batch_size = 10, epochs = 4000)
 #%%
 previsoes = classificador.predict(previsores_teste)
 
@@ -81,7 +79,7 @@ previsoes = classificador.predict(previsores_teste)
 import numpy as np
 
 # Convert probabilities to class labels using a threshold (e.g., 0.5)
-previsoes = (previsoes > 0.5).astype(int)
+previsoes = (previsoes > 0.4).astype(int)
 
 # Now, you can use previsoes with accuracy_score and other metrics:
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
